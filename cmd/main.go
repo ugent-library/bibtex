645d792c6e9c6363f172d5b6c66062bfff628856
nsteenla <nicolas.steenlant@ugent.be>
2023-12-14 11:50:54 +0100

    add string substitution

--- cmd/main.go
+++ cmd/main.go
@@ -25,12 +25,14 @@ func main() {
 		year         = {{2023}},
 	  }
 
+	@string{eng="English"}
+
 	@book{01HAKX6YXA1C4ZNYK54G1HMXEF,
 	abstract     = {{The so-called \textyen{}Mamluk\textdollar sultans \downarrow\ who ruled Egypt and Syria between the late thirteenth and early sixteenth centuries AD have often been portrayed as lacking in legitimacy due to their background as slave soldiers. Sultanic biographies written by chancery officials in the early period of the sultanate have been read as part of an effort of these sultans to legitimise their position on the throne. This book reconsiders the main corpus of six such biographies written by the historians Ibn ʿAbd al-Ẓāhir (d. 1293) and his nephew Shāfiʿ ibn ʿAlī (d. 1330) and argues that these were in fact far more complex texts. An understanding of their discourses of legitimisation needs to be embedded within a broader understanding of the multi-directional discourses operating across the texts. The study proposes to interpret these texts as "spectacles", in which authors emplotted the reign of a sultan in thoroughly literary and rhetorical fashion, making especially extensive use of textual forms prevalent in the chancery. In doing so the authors reimagined the format of the biography as a performative vehicle for displaying their literary credentials and helping them negotiate positions in the chancery and the wider courtly orbit.}},
 	author       = {{Van Den Bossche, Gowaart}},
 	isbn         = {{9783110752243}},
 	keywords     = {{Historiography,Islamic history,Mamluk,sultanate}},
-	language     = {{eng}},
+	language     = eng,
 	pages        = {{231}},
 	publisher    = {{De Gruyter}},
 	title        = {{Literary spectacles of sultanship : historiography, the chancery, and social practice in late medieval Egypt}},
--- parser.go
+++ parser.go
@@ -15,25 +15,28 @@ var (
 	reAtName       = regexp.MustCompile(`@(` + namePattern + `)`)
 	// TODO match this more efficiently
 	reKey           = regexp.MustCompile(`s*\{\s*(` + namePattern + inKeyPattern + namePattern + `)\s*,[\s\n]*|\s+\r?\s*`)
-	reField         = regexp.MustCompile(`[\s\n]*(` + namePattern + `)[\s\n]*=[\s\n]*`)
+	reFieldName     = regexp.MustCompile(`[\s\n]*(` + namePattern + `)[\s\n]*=[\s\n]*`)
 	reDigits        = regexp.MustCompile(`^\d+`)
 	reName          = regexp.MustCompile(`^` + namePattern)
+	reStringName    = regexp.MustCompile(`\{\s*(` + namePattern + `)\s*=\s*`)
 	reQuotedString  = regexp.MustCompile(`^"(([^"\\]*(\\.)*[^\\"]*)*)"`)
 	reConcatString  = regexp.MustCompile(`^\s*#\s*`)
 	reWhitespace    = regexp.MustCompile(`^\s*`)
 	reEscape        = regexp.MustCompile(`^\\.`)
-	reStringVal     = regexp.MustCompile(`^[^\\\{\}]+`) // TODO better name
+	reStringValue   = regexp.MustCompile(`^[^\\\{\}]+`)
 	reLeftBrackets  = regexp.MustCompile(`^\{+`)
 	reRightBrackets = regexp.MustCompile(`\}+$`)
 )
 
 type Parser struct {
 	scanner *bufio.Scanner
+	strings map[string]string
 }
 
 func NewParser(r io.Reader) *Parser {
 	return &Parser{
 		scanner: bufio.NewScanner(r),
+		strings: make(map[string]string),
 	}
 }
 
@@ -88,13 +91,33 @@ func (p *Parser) Next() (*Entry, error) {
 		// advance
 		eStr = eStr[m[1]:]
 
-		// TOOD handle STRING
+		// handle STRING
+		if e.Type == "STRING" {
+			m = reStringName.FindStringSubmatchIndex(eStr)
+			if m == nil {
+				return nil, errors.New("malformed string") // include more info
+			}
+			key := eStr[m[2]:m[3]]
 
-		// only add raw COMMENT and PREAMBLE
+			// advance
+			eStr = eStr[m[1]:]
+			_, val, err := p.parseString(eStr)
+			if err != nil {
+				return nil, err
+			}
+
+			p.strings[key] = val
+
+			return p.Next()
+		}
+
+		// handle COMMENT and PREAMBLE, only add raw
+		// TODO just skip?
 		if e.Type == "COMMENT" || e.Type == "PREAMBLE" {
 			return e, nil
 		}
 
+		// handle normal entry
 		m = reKey.FindStringSubmatchIndex(eStr)
 		if m == nil {
 			// include more info (see perl)
@@ -106,7 +129,7 @@ func (p *Parser) Next() (*Entry, error) {
 
 		// advance
 		eStr = eStr[m[1]:]
-		for m = reField.FindStringSubmatchIndex(eStr); m != nil; m = reField.FindStringSubmatchIndex(eStr) {
+		for m = reFieldName.FindStringSubmatchIndex(eStr); m != nil; m = reFieldName.FindStringSubmatchIndex(eStr) {
 			field := Field{Name: strings.ToLower(eStr[m[2]:m[3]])}
 			// advance
 			eStr = eStr[m[1]:]
@@ -138,23 +161,22 @@ func (p *Parser) parseString(eStr string) (string, string, error) {
 	for {
 		if m := reDigits.FindStringIndex(eStr); m != nil {
 			str += eStr[m[0]:m[1]]
-			eStr = eStr[m[1]:]
+			eStr = eStr[m[1]:] //advance
 		} else if m := reName.FindStringIndex(eStr); m != nil {
-			// TODO look up string in strings map
-			str += eStr[m[0]:m[1]]
-			eStr = eStr[m[1]:]
+			key := eStr[m[0]:m[1]]
+			str += p.strings[key]
+			eStr = eStr[m[1]:] // advance
 		} else if m := reQuotedString.FindStringSubmatchIndex(eStr); m != nil {
 			str += eStr[m[2]:m[3]]
-			eStr = eStr[m[1]:]
+			eStr = eStr[m[1]:] //advance
 		} else {
 			newEStr, val := p.extractBracketedValue(eStr)
-			// TODO remove brackets
 			str += val
 			eStr = newEStr
 		}
 
 		if m := reConcatString.FindStringIndex(eStr); m != nil {
-			eStr = eStr[m[1]:]
+			eStr = eStr[m[1]:] //advance
 			continue
 		}
 		break
@@ -192,7 +214,7 @@ func (p *Parser) extractBracketedValue(eStr string) (string, string) {
 				continue
 			}
 			break
-		} else if m := reStringVal.FindStringIndex(eStr); m != nil {
+		} else if m := reStringValue.FindStringIndex(eStr); m != nil {
 			val += eStr[m[0]:m[1]]
 			eStr = eStr[m[1]:]
 			continue
