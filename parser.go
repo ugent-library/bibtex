0c5c080ee0e2a4a737dc03e8ef4ec4748609fc06
nsteenla <nicolas.steenlant@ugent.be>
2023-12-18 16:44:53 +0100

    wip

--- parser.go
+++ parser.go
@@ -6,12 +6,11 @@ package bibtex
 
 import (
 	"bufio"
-	"fmt"
 	"io"
+	"log"
 	"regexp"
 	"strings"
-
-	"github.com/ugent-library/bibtex/latex"
+	"unicode"
 )
 
 var (
@@ -35,164 +34,253 @@ var (
 )
 
 type Parser struct {
-	scanner        *bufio.Scanner
-	entryStartLine int
-	line           int
-	strings        map[string]string
-	rest           string
+	r       *bufio.Reader
+	line    int
+	col     int
+	strings map[string]string
 }
 
 func NewParser(r io.Reader) *Parser {
 	return &Parser{
-		scanner: bufio.NewScanner(r),
+		r:       bufio.NewReader(r),
 		strings: make(map[string]string),
 	}
 }
 
 func (p *Parser) Next() (*Entry, error) {
-	scanner := p.scanner
 	buf := &strings.Builder{}
 
-	for {
-		var line string
-		if p.rest != "" {
-			line = p.rest
-			p.rest = ""
-		} else if !scanner.Scan() {
-			break
-		} else {
-			line = reStripComment.ReplaceAllString(scanner.Text(), "")
-			p.line++
-		}
-
-		if line != "" {
-			buf.WriteString(line + "\n")
-		}
-		if !strings.Contains(line, "@") {
-			continue
-		}
-
-		p.entryStartLine = p.line
-
-		e := &Entry{}
-
-		eStr := buf.String()
-
-		// get type
-		m := reAtName.FindStringSubmatchIndex(eStr)
-		if m == nil {
-			return nil, fmt.Errorf("bibtex: type not found at in entry at line %d", p.entryStartLine)
-		}
-		e.Type = strings.ToLower(eStr[m[2]:m[3]])
-
-		// read rest of entry (matches braces)
-		// TODO with malformed input without newlines between entries this reads the whole file
-		// in memory (but parses correctly)
-		startPos := m[0] - 1
-		// TODO why?
-		if startPos < 0 {
-			startPos = 0
-		}
-		// count braces
-		braceLevel := strings.Count(eStr, "{") - strings.Count(eStr, "}")
-
-		for braceLevel != 0 {
-			if !scanner.Scan() {
-				break
-			}
-			line := scanner.Text()
-			p.line++
-			braceLevel = braceLevel + strings.Count(line, "{") - strings.Count(line, "}")
-			buf.WriteString(line + "\n")
-		}
-
-		eStr = buf.String()
-
-		// raw bibtex
-		e.Raw = strings.TrimSpace(eStr[startPos:])
-
-		eStr = eStr[m[1]:] // advance
-
-		// skip @comment and @preamble
-		if e.Type == "comment" || e.Type == "preamble" {
-			return p.Next()
-		}
-
-		// handle @string
-		if e.Type == "string" {
-			m = reStringName.FindStringSubmatchIndex(eStr)
-			if m == nil {
-				return nil, fmt.Errorf("bibtex: malformed string at line %d", p.entryStartLine)
-			}
-			key := eStr[m[2]:m[3]]
-
-			eStr = eStr[m[1]:] // advance
-			_, val, err := p.parseString(eStr)
-			if err != nil {
-				return nil, err
-			}
-
-			p.strings[key] = val
-
-			return p.Next()
-		}
-
-		// handle normal entry
-		m = reKey.FindStringSubmatchIndex(eStr)
-		if m == nil {
-			return nil, fmt.Errorf("bibtex: malformed entry at line %d", p.entryStartLine)
-		}
-
-		e.Key = eStr[m[2]:m[3]]
-
-		eStr = eStr[m[1]:] // advance
-		for m = reFieldName.FindStringSubmatchIndex(eStr); m != nil; m = reFieldName.FindStringSubmatchIndex(eStr) {
-			field := Field{Name: strings.ToLower(eStr[m[2]:m[3]])}
-			eStr = eStr[m[1]:] // advance
-			newEStr, val, err := p.parseString(eStr)
-			if err != nil {
-				return nil, err
-			}
-			eStr = newEStr
-			field.RawValue = val
-			field.Value = latex.Decode(val)
-			e.Fields = append(e.Fields, field)
-
-			if field.Name == "author" {
-				e.RawAuthors = splitAuthorEditor(val)
-				e.Authors = make([]string, len(e.RawAuthors))
-				for i, name := range e.RawAuthors {
-					e.Authors[i] = latex.Decode(name)
-				}
-			} else if field.Name == "editor" {
-				e.RawEditors = splitAuthorEditor(val)
-				e.Editors = make([]string, len(e.RawEditors))
-				for i, name := range e.RawEditors {
-					e.Editors[i] = latex.Decode(name)
-				}
-			}
-
-			// skip past next comma
-			if idx := strings.Index(eStr, ","); idx > -1 {
-				eStr = eStr[idx+1:]
-			}
-		}
-
-		// handle malformed input without newlines between entries
-		if strings.Contains(eStr, "@") {
-			p.rest = eStr
-		} else {
-			p.rest = ""
-		}
-
-		return e, nil
+	err := p.skipUntil(buf, '@')
+	if err == io.EOF {
+		return nil, nil
 	}
-	if err := scanner.Err(); err != nil {
+	if err != nil {
 		return nil, err
 	}
 
-	return nil, nil
+	if err := p.readWhitespace(buf); err != nil {
+		return nil, err
+	}
+
+	if err := p.readUntil(buf, '{'); err != nil {
+		return nil, err
+	}
+
+	braceLevel := 1
+	for braceLevel != 0 {
+		c, _, err := p.r.ReadRune()
+		if err != nil {
+			return nil, err
+		}
+		if c == '{' {
+			braceLevel++
+		} else if c == '}' {
+			braceLevel--
+		}
+		_, err = buf.WriteRune(c)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	entry := &Entry{
+		Raw: buf.String(),
+	}
+
+	log.Printf("ENTRY: %+v\n----", entry)
+
+	return entry, nil
 }
+
+func (p *Parser) skipUntil(buf *strings.Builder, char rune) error {
+	for {
+		c, _, err := p.r.ReadRune()
+		if err != nil {
+			return err
+		}
+		if c == char {
+			_, err := buf.WriteRune(c)
+			return err
+		}
+	}
+}
+
+func (p *Parser) readWhitespace(buf *strings.Builder) error {
+	for {
+		c, _, err := p.r.ReadRune()
+		if err != nil {
+			return err
+		}
+		if unicode.IsSpace(c) {
+			if _, err := buf.WriteRune(c); err != nil {
+				return err
+			}
+		} else {
+			return p.r.UnreadRune()
+		}
+	}
+}
+
+func (p *Parser) readUntil(buf *strings.Builder, char rune) error {
+	for {
+		c, _, err := p.r.ReadRune()
+		if err != nil {
+			return err
+		}
+		if _, err := buf.WriteRune(c); err != nil {
+			return err
+		}
+		if c == char {
+			return nil
+		}
+	}
+}
+
+// TODO read rune by rune, not line by line
+// func (p *Parser) Next() (*Entry, error) {
+// 	scanner := p.scanner
+// 	buf := &strings.Builder{}
+
+// 	for {
+// 		var line string
+// 		if p.rest != "" {
+// 			line = p.rest
+// 			p.rest = ""
+// 		} else if !scanner.Scan() {
+// 			break
+// 		} else {
+// 			line = reStripComment.ReplaceAllString(scanner.Text(), "")
+// 			p.line++
+// 		}
+
+// 		if line != "" {
+// 			buf.WriteString(line + "\n")
+// 		}
+// 		if !strings.Contains(line, "@") {
+// 			continue
+// 		}
+
+// 		p.entryStartLine = p.line
+
+// 		e := &Entry{}
+
+// 		eStr := buf.String()
+
+// 		// get type
+// 		m := reAtName.FindStringSubmatchIndex(eStr)
+// 		if m == nil {
+// 			return nil, fmt.Errorf("bibtex: type not found at in entry at line %d", p.entryStartLine)
+// 		}
+// 		e.Type = strings.ToLower(eStr[m[2]:m[3]])
+
+// 		// read rest of entry (matches braces)
+// 		// TODO with malformed input without newlines between entries this reads the whole file
+// 		// in memory (but parses correctly)
+// 		startPos := m[0] - 1
+// 		// TODO why?
+// 		if startPos < 0 {
+// 			startPos = 0
+// 		}
+// 		// count braces
+// 		braceLevel := strings.Count(eStr, "{") - strings.Count(eStr, "}")
+
+// 		for braceLevel != 0 {
+// 			if !scanner.Scan() {
+// 				break
+// 			}
+// 			line := scanner.Text()
+// 			p.line++
+// 			braceLevel = braceLevel + strings.Count(line, "{") - strings.Count(line, "}")
+// 			buf.WriteString(line + "\n")
+// 		}
+
+// 		eStr = buf.String()
+
+// 		// raw bibtex
+// 		e.Raw = strings.TrimSpace(eStr[startPos:])
+
+// 		eStr = eStr[m[1]:] // advance
+
+// 		// skip @comment and @preamble
+// 		if e.Type == "comment" || e.Type == "preamble" {
+// 			return p.Next()
+// 		}
+
+// 		// handle @string
+// 		if e.Type == "string" {
+// 			m = reStringName.FindStringSubmatchIndex(eStr)
+// 			if m == nil {
+// 				return nil, fmt.Errorf("bibtex: malformed string at line %d", p.entryStartLine)
+// 			}
+// 			key := eStr[m[2]:m[3]]
+
+// 			eStr = eStr[m[1]:] // advance
+// 			_, val, err := p.parseString(eStr)
+// 			if err != nil {
+// 				return nil, err
+// 			}
+
+// 			p.strings[key] = val
+
+// 			return p.Next()
+// 		}
+
+// 		// handle normal entry
+// 		m = reKey.FindStringSubmatchIndex(eStr)
+// 		if m == nil {
+// 			return nil, fmt.Errorf("bibtex: malformed entry at line %d", p.entryStartLine)
+// 		}
+
+// 		e.Key = eStr[m[2]:m[3]]
+
+// 		eStr = eStr[m[1]:] // advance
+// 		for m = reFieldName.FindStringSubmatchIndex(eStr); m != nil; m = reFieldName.FindStringSubmatchIndex(eStr) {
+// 			field := Field{Name: strings.ToLower(eStr[m[2]:m[3]])}
+// 			eStr = eStr[m[1]:] // advance
+// 			newEStr, val, err := p.parseString(eStr)
+// 			if err != nil {
+// 				return nil, err
+// 			}
+// 			eStr = newEStr
+// 			field.RawValue = val
+// 			field.Value = latex.Decode(val)
+// 			e.Fields = append(e.Fields, field)
+
+// 			if field.Name == "author" {
+// 				e.RawAuthors = splitAuthorEditor(val)
+// 				e.Authors = make([]string, len(e.RawAuthors))
+// 				for i, name := range e.RawAuthors {
+// 					e.Authors[i] = latex.Decode(name)
+// 				}
+// 			} else if field.Name == "editor" {
+// 				e.RawEditors = splitAuthorEditor(val)
+// 				e.Editors = make([]string, len(e.RawEditors))
+// 				for i, name := range e.RawEditors {
+// 					e.Editors[i] = latex.Decode(name)
+// 				}
+// 			}
+
+// 			// skip past next comma
+// 			if idx := strings.Index(eStr, ","); idx > -1 {
+// 				eStr = eStr[idx+1:]
+// 			}
+// 		}
+
+// 		// handle malformed input without newlines between entries
+// 		if strings.Contains(eStr, "@") {
+// 			p.rest = eStr
+// 		} else {
+// 			p.rest = ""
+// 		}
+
+// 		return e, nil
+// 	}
+// 	if err := scanner.Err(); err != nil {
+// 		return nil, err
+// 	}
+
+// 	return nil, nil
+// }
 
 func (p *Parser) parseString(eStr string) (string, string, error) {
 	buf := &strings.Builder{}
